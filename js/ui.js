f5de8514648809541644ee3dba113172865ddf63
aa.snigirev <aa.snigirev@tensor.ru>
2021-07-14 15:27:09 +0500

    bootstrap ui

--- js/ui.js
+++ js/ui.js
@@ -1,37 +1,38 @@
 export default class Ui {
     constructor(selector, game) {
-        this._el = $(selector);
+        this._el = document.querySelector(selector);
+
         this._game = game;
 
-        this._rb = this._el.find('button.ready').button();
-        this._cb = this._el.find('button.connect').button();
-        this._cbt = this._el.find('button.connectTo').button();
+        this._cb = this._el.querySelector('button#connect');
+        this._rb = this._el.querySelector('button#ready');
+        this._cbt = this._el.querySelector('button#connectTo');
 
-        this._rb.click(() => {
-            this._game.setReady();
-        });
-
-        this._cb.click(() => {
+        this._cb.addEventListener('click', () => {
             this._game.connect();
-        });
+        })
 
-        this._cbt.click(() => {
-            const uuid = this._el.find('input.connectToInput')[0].value;
+        this._rb.addEventListener('click', () => {
+            this._game.setReady();
+        })
+
+        this._cbt.addEventListener('click', () => {
+            const uuid = this._el.querySelector('input#connectToInput').value;
             if (uuid) {
                 this._game.connectTo(uuid);
             }
-        });
+        })
     }
 
     drawPlayers() {
-        this._el.find('.items').html(this._game.players.map(pl => `<div>${pl.uuid} - ${pl.state}</div>`).join(''));
+        this._el.querySelector('.items').innerHTML = this._game.players.map(pl => `<li>${pl.uuid} <span class="badge bg-${pl.state === 'ready' ? 'primary' : 'secondary'}">${pl.state}</span></li>`).join('');
     }
 
     updateButtons() {
         if (this._game.state === 'connected') {
-            this._cb.button('disable');
+            this._cb.setAttribute('disabled', 'disabled');
         } else {
-            this._cb.button('enable');
+            this._cb.removeAttribute('disabled')
         }
     }
 }
